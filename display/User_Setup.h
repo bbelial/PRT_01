19026dffe97dcec06ed0cfb2692a6a3cd8f8844f
Belial <bbelial@tuta.io>
2026-05-24 23:50:58 +0700

    Remove MOSI pin definition for TFT_eSPI

--- display/User_Setup.h
+++ display/User_Setup.h
@@ -209,7 +209,7 @@
 // For ESP32 Dev board (only tested with ILI9341 display)
 // The hardware SPI can be mapped to any pins
 
-#define TFT_MISO 19
+// #define TFT_MISO 19
 #define TFT_MOSI 23
 #define TFT_SCLK 18
 #define TFT_CS   15  // Chip select control pin
